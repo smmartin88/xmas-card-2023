9edeaf47e2acf01c6c4ff2305120a0ac39eea468
smmartin88 <smmartin44@gmail.com>
2023-12-10 21:42:26 -0500

    adding placeholder carousel

--- xmas-card/components/Tabs.tsx
+++ xmas-card/components/Tabs.tsx
@@ -6,6 +6,8 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Wordle from './wordle/wordle';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -15,6 +17,7 @@ interface TabPanelProps {
 
 function CustomTabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
+
 
   return (
     <div
@@ -47,19 +50,32 @@ export default function BasicTabs() {
     setValue(newValue);
   };
 
+  const handleDragStart = (e: any) => e.preventDefault();
+
+  const items = [
+    <img src="assets/one.JPEG" onDragStart={handleDragStart} role="presentation" />,
+    <img src="assets/two.JPEG" onDragStart={handleDragStart} role="presentation" />,
+    <img src="assets/three.JPEG" onDragStart={handleDragStart} role="presentation" />,
+  ];
+
   return (
     <div>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Home" {...a11yProps(0)} />
-          <Tab label="Wordle" {...a11yProps(1)} />
-          <Tab label="Connections" {...a11yProps(2)} />
-          <Tab label="Memory" {...a11yProps(3)} />
-          <Tab label="About" {...a11yProps(4)} />
+        <Tabs TabIndicatorProps={{style: {background:'#710627'}}} value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label={<span style={{ color: '#710627' }}>Home</span>} {...a11yProps(0)} />
+          <Tab label={<span style={{ color: '#710627' }}>Wordle</span>} {...a11yProps(1)} />
+          <Tab label={<span style={{ color: '#710627' }}>Connections</span>} {...a11yProps(2)} />
+          <Tab label={<span style={{ color: '#710627' }}>Memory</span>} {...a11yProps(3)} />
+          <Tab label={<span style={{ color: '#710627' }}>About</span>} {...a11yProps(4)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        <div className='text-center'>
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-700 md:text-5xl lg:text-6xl">Merry Christmas from the Martins! 🎅</h1>
+            <div style={{ width: '30%' }}>
+                <AliceCarousel mouseTracking items={items} autoPlay infinite autoHeight animationType='fadeout' autoPlayInterval={3000} />
+            </div>
+        </div>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         <Wordle/>
